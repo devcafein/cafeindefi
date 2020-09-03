f5a0a997e425d0589c1f5aad0702e2ea3cb7400c
devcafein <hotroqpcoffe@gmail.com>
2020-09-03 15:27:43 +0700

    okie

---
+++ client/build/precache-manifest.3bcc34bc810eec2d229f9adda01b2591.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ecda4bdfd326a4b05757242ac4b76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0bf2ebacaed6cb9b11c",
+    "url": "/static/css/main.0a0e8a4a.chunk.css"
+  },
+  {
+    "revision": "7d4ce7641633fae2f97c",
+    "url": "/static/js/2.0a129bbd.chunk.js"
+  },
+  {
+    "revision": "a0bf2ebacaed6cb9b11c",
+    "url": "/static/js/main.1bd4b454.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "ddffeeccd93bfa8838233c4c19073785",
+    "url": "/static/media/Nyan_pump.ddffeecc.gif"
+  },
+  {
+    "revision": "decb4b682f27410f68bed3cfd25567db",
+    "url": "/static/media/Nyan_stake.decb4b68.gif"
+  },
+  {
+    "revision": "1859696178ada5cf499e4b26a62d0597",
+    "url": "/static/media/Nyan_vote.18596961.gif"
+  },
+  {
+    "revision": "38ec77e47a3a2a27bced6860ae1252e6",
+    "url": "/static/media/nyan-small.38ec77e4.gif"
+  },
+  {
+    "revision": "a823024f323f3cd4345044aa8c51d5ac",
+    "url": "/static/media/telegram.a823024f.png"
+  }
+]);
